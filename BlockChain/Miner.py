37d79ffcc23592e2b1de37866f69287a776f07b3
Grant Dennany <gdennany@iu.edu>
2020-03-27 16:58:06 -0400

    start bulding miner using threads

--- BlockChain/Miner.py
+++ BlockChain/Miner.py
@@ -2,10 +2,12 @@ import SocketUtils
 import Transaction
 import TxBlock
 import Signatures
+import threading
 
-wallets = ['localhost']
+wallets = [('localhost', 5005)]
 txList = []
 headBlocks = [None]
+breakNow = False
 
 def findLongestBlockChain():
     longest = -1
@@ -21,11 +23,14 @@ def findLongestBlockChain():
             longest = thisLen
     return longHead
 
-def minerServer(myIP, walletList, myPublic):
+def minerServer(myAddr):
+    global txList
+    global breakNow
+    myIp, myPort = myAddr
     #Open Server Connection
-    server = SocketUtils.newServerConnection('localhost')
+    server = SocketUtils.newServerConnection(myIp, myPort)
     #recieve two transactions wallet
-    for i in range(10):
+    while not breakNow: #for i in range(10):
         print("Finding Nonce...")
         newTx = SocketUtils.recvObj(server)
         if isinstance(newTx, Transaction.Tx):
@@ -38,7 +43,9 @@ def minerServer(myIP, walletList, myPublic):
     newBlock = TxBlock.TxBlock(findLongestBlockChain())
     newBlock.addTx(txList[0])
     newBlock.addTx(txList[1])
-    
+    return False
+
+def nonceFinder(walletList, minerPubl):
     #compute and add mining reward
     totalIn, totalOut = newBlock.count_totals()
     mineReward = Transaction.Tx()
@@ -61,7 +68,72 @@ def minerServer(myIP, walletList, myPublic):
         SocketUtils.sendObj(ipAddr, newBlock, 5006)
     headBlocks.remove(newBlock.previousBlock)
     headBlocks.append(newBlock)
-    return False
+    breakNow = True
+    return True
 
-myPriv, myPubl = Signatures.generate_keys()
-minerServer('localhost', wallets, myPubl)
+if __name__ == "__main__":
+    myPriv, myPubl = Signatures.generate_keys()
+    
+    t1 = threading.Thread(target = minerServer, args = (('localhost', 5005),))
+    t2 = threading.Thread(target = nonceFinder, args = (wallets, myPubl,))
+    
+    t1.start()
+    t2.start()
+
+    priv1, publ1 = Signatures.generate_keys()
+    priv2, publ2 = Signatures.generate_keys()
+    priv3, publ3 = Signatures.generate_keys()
+
+    Tx1 = Transaction.Tx()
+    Tx2 = Transaction.Tx()
+
+    Tx1.add_input(publ1, 4.0)
+    Tx1.add_input(publ2, 1.0)
+    Tx1.add_output(publ3, 4.8)
+    Tx2.add_input(publ3, 4.0)
+    Tx2.add_output(publ2, 4.0)
+    Tx2.add_reqd(publ1)
+
+    Tx1.sign(priv1)
+    Tx1.sign(priv2)
+    Tx2.sign(priv3)
+    Tx2.sign(priv1)
+
+    try:
+        SocketUtils.sendObj('localhost', Tx1)
+        print("Sent Tx1")
+        SocketUtils.sendObj('localhost', Tx2)
+        print("Sent Tx2")
+    except:
+        print("Error: Connection unsuccessful")
+
+    server = SocketUtils.newServerConnection('localhost', 5006)
+    for i in range(30):
+        newBlock = SocketUtils.recvObj(server)
+        if newBlock:
+            break
+    server.close()
+
+    if newBlock.is_valid():
+        print("Success, block is valid")
+    else:
+        print("Error, invalid block")
+    if newBlock.good_nonce():
+        print("Success, nonce is valid")
+    else:
+        print("Error, invalid nocne")
+
+    for tx in newBlock.data:
+        try:
+            if tx.inputs[0][0] == publ1 and tx.inputs[0][1] == 4.0:
+                print("Tx1 is present")
+            if tx.inputs[0][0] == publ3 and tx.inputs[0][1] == 4.0:
+                print("Tx2 is present")  
+        except IndexError:
+            pass
+
+
+    t1.join()
+    t2.join()
+
+    print("Done")
